8ea800cbfcb33148ccc5b308016f8809a2469c0e
Sara Montagud <43246362+Aforina@users.noreply.github.com>
2025-01-17 00:05:22 +0100

    feat: finish app

---
+++ src/config/projects.ts
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    id: "project1",
+    title: "Palette Generator",
+    description:
+      "Palette Generator is an intuitive tool designed to help creatives build stunning color palettes with ease.",
+    src: "/projects/project-1.png",
+    alt: "Palette Generator Cover",
+    link: "https://palette-generator-indol.vercel.app/?color=%233ba996",
+  },
+  {
+    id: "project2",
+    title: "Favicon Generator",
+    description:
+      "Favicon Maker is an intuitive tool designed to help web creators craft high-quality favicons with ease.",
+    src: "/projects/project-2.png",
+    alt: "Favicon Generator Cover",
+    link: "https://favicons.afor.digital",
+  },
+  {
+    id: "project3",
+    title: "Pattern Generator",
+    description:
+      "Pattern Generator is an intuitive tool designed to help designers create seamless patterns effortlessly.",
+    src: "/projects/project-3.png",
+    alt: "Background Pattern Cover",
+    link: "https://pattern-generator-woad.vercel.app",
+  },
+];
